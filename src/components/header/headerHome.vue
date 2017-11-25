6168f0023051288ccca37a92b899728396be3039
apple <apple@appledeMacBook-Pro.local>
2017-11-25 10:26:30 +0800

    同步样式

--- src/components/header/headerHome.vue
+++ src/components/header/headerHome.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="header-home-container"></div>
+  <div class="header-home-container" flex="">
+    <div class="header-logo iconfont" flex-box="0">&#xe614;</div>
+    <div class="header-search" flex-box="1">
+      <span class="header-search-placeholder"><i class="iconfont iconfont-search">&#xe60d;</i>寻找宝贝店铺</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,6 +23,31 @@ export default {
         max-width: 7.5rem;
         height: .95rem;
         background-color: #FE5400;
+        padding: .12rem .12rem .12rem 0;
+        .header-logo {
+          font-size: .7rem;
+          color: #fff;
+        }
+        .header-search {
+          width: 100%;
+          box-sizing: border-box;
+          background-color: #B52600;
+          text-align: center;
+          border-radius: .1rem;
+          height: .7rem;
+          line-height: .7rem;
+          .header-search-placeholder {
+            color: #fff;
+            font-size: .3rem;
+            .iconfont-search {
+              -webkit-font-smoothing: antialiased;
+              -webkit-text-stroke-width: 0.2px;
+              -moz-osx-font-smoothing: grayscale;
+              font-size: .3rem;
+              margin-right: 5px;
+            }
+          }
+        }
     }
 </style>
 
