a808e0d6c563095625f0d22c105acc6cd0460dac
sosout <sosout@139.com>
2017-12-18 17:23:34 +0800

    首页导航菜单

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -9,6 +9,48 @@
             <img :src="ads.url" :alt="ads.name" />
           </mt-swipe-item>
         </mt-swipe>
+        <div class="home-menu">
+          <div class="menu-item">
+            <div class="item-img img1"></div>
+            <div class="item-title">天猫</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-img img2"></div>
+            <div class="item-title">聚划算</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-img img3"></div>
+            <div class="item-title">天猫国际</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-img img4"></div>
+            <div class="item-title">外卖</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-img img5"></div>
+            <div class="item-title">天猫超市</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-img img6"></div>
+            <div class="item-title">充值中心</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-img img7"></div>
+            <div class="item-title">飞猪旅行</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-img img8"></div>
+            <div class="item-title">领金币</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-img img9"></div>
+            <div class="item-title">拍卖</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-img img10"></div>
+            <div class="item-title">分类</div>
+          </div>
+        </div>
       </div>
     </div>
     <footer-tabbar :current='"home"' />
@@ -69,6 +111,65 @@ export default {
         width: 100%;
         height: 100%;
         display: block;
+      }
+    }
+    .home-menu {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 0 .2rem;
+      .menu-item {
+        width: 20%;
+        text-align: center;
+        margin-top: .2rem;
+        .item-img {
+          box-sizing: border-box;
+          height: 1rem;
+          &.img1 {
+            background: url(http://sosout.com/static/images/tb/tm.png) no-repeat center center;
+            background-size: cover;
+          }
+          &.img2 {
+            background: url(http://sosout.com/static/images/tb/jhs.png) no-repeat center center;
+            background-size: cover;
+          }
+          &.img3 {
+            background: url(http://sosout.com/static/images/tb/tmgj.png) no-repeat center center;
+            background-size: cover;
+          }
+          &.img4 {
+            background: url(http://sosout.com/static/images/tb/wm.png) no-repeat center center;
+            background-size: cover;
+          }
+          &.img5 {
+            background: url(http://sosout.com/static/images/tb/tmcs.png) no-repeat center center;
+            background-size: cover;
+          }
+          &.img6 {
+            background: url(http://sosout.com/static/images/tb/czzx.png) no-repeat center center;
+            background-size: cover;
+          }
+          &.img7 {
+            background: url(http://sosout.com/static/images/tb/fzlx.png) no-repeat center center;
+            background-size: cover;
+          }
+          &.img8 {
+            background: url(http://sosout.com/static/images/tb/ljb.png) no-repeat center center;
+            background-size: cover;
+          }
+          &.img9 {
+            background: url(http://sosout.com/static/images/tb/pm.png) no-repeat center center;
+            background-size: cover;
+          }
+          &.img10 {
+            background: url(http://sosout.com/static/images/tb/fl.png) no-repeat center center;
+            background-size: cover;
+          }
+        }
+        .item-title {
+          color: #666;
+          font-size: .3rem;
+          margin-top: .1rem;
+        }
       }
     }
   }
