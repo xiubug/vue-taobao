f416e8076d4558657e1d8ddff178d3c9fcac305a
sosout <sosout@139.com>
2018-01-04 17:42:26 +0800

    增加启动图

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,25 @@
 
 <script>
 import 'flex.css'
-export default {}
+export default {
+  data () {
+    return {}
+  },
+  mounted () {
+    this.appShow()
+  },
+  methods: {
+    appShow () {
+      const { START_TIME } = window
+      const END_TIME = new Date().getTime() // 结束时间
+      const diffTime = END_TIME - START_TIME
+      const timer = setTimeout(() => {
+        clearTimeout(timer)
+        document.querySelector('.app-loading').className += ' app-loading-hide'
+      }, diffTime > 2000 ? 0 : 2000 - diffTime)
+    }
+  }
+}
 </script>
 
 <style lang="less">
